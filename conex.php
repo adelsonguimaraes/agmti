3242412756ed279dc82c5171feec98ac49685b04
adelsonguimaraes <adelsonguimaraes@gmail.com>
2016-06-09 18:25:26 -0400

    .

--- conex.php
+++ conex.php
@@ -19,6 +19,8 @@
 		echo "Erro: " . mysqli_error($con);
 	}
 
+	createAutoload();
+
 	while ($row = mysqli_fetch_object($result)) {
 
 		$sql = "SHOW COLUMNS FROM " . $row->table;
@@ -522,5 +524,54 @@
 		$escreve = fwrite($fp, $text, strlen($text));
 
 		fclose($fp);
+	}
+
+	function createAutoload () {
+
+		if(!file_exists('src')) mkdir('src');
+		if(!file_exists('src/rest')) mkdir('src/rest');
+		
+		$fp = fopen("src/rest/autoload.php", "a");
+		
+		$text = "<?php\n";
+		$text .= "// autoload \n\n";
+
+		$text .= "Trata requisição\n";
+		$text .= "if(!\$_POST){\n";
+		$text .= "	if(\$_GET) {\$_POST = \$_GET;}\n";
+		$text .= "	else{\$_POST =  file_get_contents ( 'php://input' );}";
+		$text .= "}\n\n";
+
+		$text .= "// conexao\n";
+		$text .= "require_once(\"../Conexao.php\");\n\n";
+
+		$text .= "// carrega class";
+		$text .= "function carregaClasses(\$class){\n";
+		$text .= "	//Verifica se existe Control no nome da classe\n";
+		$text .= "	if(strrpos(\$class, \"Control\")) {\n";
+		$text .= "		require_once(\"../control/\".\$class.\".php\");\n";
+		$text .= "	}\n\n";
+		$text .= "	//Verifica se existe DAO no nome da classe";
+		$text .= "	else if(strrpos(\$class, \"DAO\")) {\n";
+		$text .= "		\$bean = strtolower(substr(\$class, 0, strrpos(\$class, \"DAO\")));\n";
+		$text .= "		require_once \"../model/\".\$bean.\"/\".\$class.\".php\";{\n";
+		$text .= " 	//se nao for control ou dao é model\n";
+		$text .= " 	}else{\n";
+		$text .= "		\$bean = strtolower(\$class);\n";
+		$text .= "		require_once \"../model/\".\$bean.\"/\".\$class.\".php\";\n";
+		$text .= "	}";
+		$text .= "}\n\n";
+
+		$text .= "//chama autoload\n";
+		$text .= "spl_autoload_register(\"carregaClasses\");\n";
+
+		$text .= "spl_autoload_register(\"carregaClasses\");\n";
+
+		$text .= "?>";
+
+		$escreve = fwrite($fp, $text, strlen($text));
+
+		fclose($fp);
+
 	}
 ?>
